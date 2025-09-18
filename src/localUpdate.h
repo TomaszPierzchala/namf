d7b7df93b11b9b1026fb2523bc8ac76b376bafb0
TomaszPierzchala <t.pierzchala@gmail.com>
2025-09-18 23:17:14 +0200

    started sending over HTTPS but timeout

--- src/localUpdate.h
+++ src/localUpdate.h
@@ -28,9 +28,92 @@ t_httpUpdate_return tryUpdate(const String host, const String port, const String
 #include <ESP8266httpUpdate.h>
 #include "helpers.h"
 t_httpUpdate_return tryUpdate(const String host, const String port, const String path, const String ver) {
-    WiFiClient client;
-    Serial.println(ver);
-    t_httpUpdate_return ret = ESPhttpUpdate.update(client, host, port.toInt(), path, ver);
+    debug_out(F(">>>>> HOST: "), DEBUG_MIN_INFO,false); debug_out(host, DEBUG_MIN_INFO,true);
+    debug_out(F(">>>>> PORT: "), DEBUG_MIN_INFO,false); debug_out(port, DEBUG_MIN_INFO,true);
+    debug_out(F(">>>>> PATH: "), DEBUG_MIN_INFO,false); debug_out(path, DEBUG_MIN_INFO,true);
+
+    WiFi.setSleepMode(WIFI_NONE_SLEEP);
+    // 1. same/one pointer for both clients
+    std::unique_ptr<WiFiClient> client;
+
+    // 2. choose the right client depending on the port
+    if (port == F("443")) {
+        debug_out(F(">>>>> PORT: "), DEBUG_MIN_INFO,false); debug_out(port, DEBUG_MIN_INFO,true);
+        auto httpsClient = new BearSSL::WiFiClientSecure();
+        httpsClient->setInsecure();   // DEV only, at the production stage the certificate should be verified
+        httpsClient->setBufferSizes(8192, 512);
+        client.reset(httpsClient);
+    } else {
+        debug_out(F(">>>>> ELSE PORT: "), DEBUG_MIN_INFO,false); debug_out(port, DEBUG_MIN_INFO,true);
+        client.reset(new WiFiClient());
+    }
+
+    Serial.println("Version: " + ver);
+    client->setTimeout(90000); // 20 seconds -> ..
+    ESPhttpUpdate.rebootOnUpdate(false); 
+    // t_httpUpdate_return ret = ESPhttpUpdate.update(*client, host, port.toInt(), path, ver);
+    
+    debug_out(F("Free heap: "), DEBUG_MIN_INFO,false); debug_out(String(ESP.getFreeHeap()), DEBUG_MIN_INFO,true);
+    HTTPClient http;
+    http.begin(*client, host, port.toInt(), path, false);
+    // http.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
+    String firmwareUrl = String("http") + (true ? "s" : "") + "://" + host + ":" + String(port) + path;
+    // Serial.printf("Sprawdzam URL: %s\n", firmwareUrl);
+    debug_out(F("Sprawdzam URL: "), DEBUG_MIN_INFO,false); debug_out(firmwareUrl, DEBUG_MIN_INFO,true);
+
+    t_httpUpdate_return ret;
+    int httpCode = http.GET();
+
+    debug_out(F("Kod HTTP: "), DEBUG_MIN_INFO,false); debug_out(String(httpCode), DEBUG_MIN_INFO,true);
+    if (httpCode == HTTP_CODE_FOUND || httpCode == HTTP_CODE_MOVED_PERMANENTLY || httpCode == HTTP_CODE_SEE_OTHER) {
+      // pobierz nagłówek Location
+      String newUrl = http.getLocation();//("Location");
+      debug_out(F("Redirect → "), DEBUG_MIN_INFO,false); debug_out(newUrl.c_str(), DEBUG_MIN_INFO,true);
+
+      if (!client) {
+        // Serial.println("Błąd: client NULL!");
+        debug_out(F("Błąd: client NULL"), DEBUG_MIN_INFO, true);
+      }
+        if (newUrl.length() == 0) {
+            debug_out(F("Błąd: pusty URL"), DEBUG_MIN_INFO, true);
+        }
+      // teraz robimy update z nowym URL
+      ret = ESPhttpUpdate.update(*client, newUrl);
+
+      switch (ret) {
+        case HTTP_UPDATE_FAILED:
+            debug_out(F("Update FAIL. Err : ("), DEBUG_MIN_INFO,false); debug_out(String(ESPhttpUpdate.getLastError()), DEBUG_MIN_INFO,false);
+            debug_out(F("): "), DEBUG_MIN_INFO,false); debug_out(ESPhttpUpdate.getLastErrorString().c_str(), DEBUG_MIN_INFO,true);
+          break;
+
+        case HTTP_UPDATE_NO_UPDATES:
+          debug_out(F("Brak aktualizacji."), DEBUG_MIN_INFO, true);
+          break;
+
+        case HTTP_UPDATE_OK:
+          debug_out(F("Update OK, reboot!"), DEBUG_MIN_INFO, true);
+          ESP.restart();
+          break;
+      }
+    } else if (httpCode == HTTP_CODE_OK) {
+        debug_out(F("URL daje 200 OK — można update bezpośrednio"), DEBUG_MIN_INFO, true);
+      ret = ESPhttpUpdate.update(*client, firmwareUrl);
+      // obsługa jak wyżej
+    } else {
+        debug_out(F("Nieoczekiwany kod HTTP: "), DEBUG_MIN_INFO,false); debug_out(String(httpCode), DEBUG_MIN_INFO,true);
+    }
+    http.end();
+  
+
+
+    // return handleUpdate(http, ver, false);
+    // t_httpUpdate_return ret = ESPhttpUpdate.update(http, ver); // <<<<<
+          // wyłącz auto-reboot
+// auto ret = ESPhttpUpdate.update(client, url, currentVersion);
+    debug_out(F("Update return code: "), DEBUG_MIN_INFO,false); debug_out(String(ret), DEBUG_MIN_INFO,true);
+    debug_out(F("Last error: "), DEBUG_MIN_INFO,false); debug_out(String(ESPhttpUpdate.getLastError()), DEBUG_MIN_INFO,true);
+    debug_out(F("Error msg: "), DEBUG_MIN_INFO,false); debug_out(ESPhttpUpdate.getLastErrorString().c_str(), DEBUG_MIN_INFO,true);
+    debug_out(F(">>>>> UPDATE result: "), DEBUG_MIN_INFO,false); debug_out(String(ret), DEBUG_MIN_INFO,true);
     return ret;
 };
 #endif
@@ -38,6 +121,8 @@ t_httpUpdate_return tryUpdate(const String host, const String port, const String
 t_httpUpdate_return tryUpdate(String const ver) {
     String host;
     String url;
+    debug_out(F(">>>>> UPDATE CHANEL: "), DEBUG_MIN_INFO,false);
+    debug_out(String(cfg::update_channel), DEBUG_MIN_INFO,true);
     switch(cfg::update_channel) {
         case UPDATE_CHANNEL_ALFA:
             host = String(UPDATE_HOST_ALFA);
